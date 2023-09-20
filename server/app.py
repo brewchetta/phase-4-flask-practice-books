f1bee9495fc0bd2c06464e4214c5fe37212eb916
brewchetta <chett.tiller@gmail.com>
2023-09-20 15:16:50 -0400

    partial solution

--- server/app.py
+++ server/app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-from models import db # import your models here!
+from models import db, Author, Book, Publisher
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -19,7 +19,54 @@ db.init_app(app)
 def index():
     return "Hello world"
 
-# write your routes here!
+
+# GET author by id
+@app.get('/authors/<int:id>')
+def author_by_id( id ):
+    try:
+        author = Author.query.filter(Author.id == id).first()
+        return jsonify( author.to_dict() ), 200
+    except:
+        return jsonify({'error': 'Author not found'}), 404
+
+
+# GET books
+@app.get('/books')
+def all_books():
+    books = Book.query.all()
+    books_to_dict = [ book.to_dict() for book in books ]
+    return jsonify( books_to_dict ), 200
+
+# GET publisher by id - SKIPPING
+
+# POST book
+@app.post('/books')
+def create_book():
+    try:
+        data = request.json
+        new_book = Book(**data)
+        db.session.add(new_book)
+        db.session.commit()
+        return jsonify( new_book.to_dict() ), 201
+    except:
+        return jsonify( {'error': 'Invalid data'} ), 406
+
+
+# DELETE author by id
+@app.delete('/author/<int:id>')
+def delete_author( id ):
+    try:
+        author = Author.query.filter(Author.id == id).first()
+
+        for book in author.books:
+            db.session.delete(book)
+
+        db.session.delete(author)
+        db.session.commit()
+        return {}, 204
+    except:
+        return jsonify( {'error': 'Author not found'} ), 404
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -9,4 +10,73 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
-# write your models here!
+
+class Book(db.Model):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False, unique=True)
+    page_count = db.Column(db.Integer, nullable=False, default=1)
+
+    author_id = db.Column(db.Integer, db.ForeignKey('authors.id'))
+    publisher_id = db.Column(db.Integer, db.ForeignKey('publishers.id'))
+
+    @validates('page_count')
+    def validate_pages(self, key, value):
+        if value > 0:
+            return value
+        else:
+            raise ValueError(f'{key} must be greater than 0')
+
+    author = db.relationship('Author', back_populates='books')
+    publisher = db.relationship('Publisher', back_populates='books')
+
+    # serialize_rules = ('-author.books', '-publisher.books')
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "page_count": self.page_count,
+            "publisher": self.publisher.to_dict_without_books()
+        }
+
+
+class Publisher(db.Model, SerializerMixin):
+    __tablename__ = 'publishers'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    founding_year = db.Column(db.Integer, nullable=False, default=2000)
+
+    @validates('founding_year')
+    def validate_year(self, key, value):
+        if 2023 >= value >= 1600:
+            return value
+        else:
+            raise ValueError(f'{key} must be between 1600 and 2023')
+
+    books = db.relationship('Book', back_populates='publisher')
+    authors = association_proxy('books', 'author')
+
+    serialize_rules = ('-books.publisher',)
+
+    def to_dict_without_books(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "founding_year": self.founding_year
+        }
+
+
+class Author(db.Model, SerializerMixin):
+    __tablename__ = 'authors'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    pen_name = db.Column(db.String)
+
+    books = db.relationship('Book', back_populates='author')
+    publishers = association_proxy('books', 'publisher')
+
+    serialize_rules = ('-books.author',)
